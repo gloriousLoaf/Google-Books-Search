a213b8ed226164b8084568cd627a984d178d0e97
gloriousLoaf <davidmetcalfwork@gmail.com>
2020-08-29 14:04:51 -0700

    Installed CRA, deleted most boilerplate, created basic file structure.

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello! I deleted the CRA boilerplate. You are looking at a placeholder...
+      </header>
+    </div>
+  );
+}
+
+export default App;
